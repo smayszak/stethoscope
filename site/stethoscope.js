81d9c74ae14cdd8beade6eef7acc3fbf90e3c37c
jason dennis <jasonmdennis@jason-denniss-macbook-4.local>
2014-07-18 15:56:36 -0700

    Updated UI and backend to listen for incoming messages and display.

--- site/stethoscope.js
+++ site/stethoscope.js
@@ -1,3 +1,6 @@
+Messages = new Meteor.Collection("messages");
+
+
 if (Meteor.isClient) {
   Template.hello.greeting = function () {
     return "Welcome to stethoscope.";
@@ -7,21 +10,37 @@ if (Meteor.isClient) {
     'click input': function () {
       // template data, if any, is available in 'this'
       if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+      { 
+     	console.log("You pressed the button and did something");
+  	   } 
     }
   });
+  
+  Meteor.subscribe("messages");
+  
+  Template.ajaxResponse.messages = function(){
+	return Messages.find();
+  };
+    
 }
 
-Meteor.methods({
-  createHealthCheckResult: function (data) {
-    console.log('data was posted');
-    return {"status":"ok"};
-  }
-});
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
-    console.log("started");
+    console.log("app started");
+    //clear the old messages
+    Messages.remove({});
   });
+  
+  
+	Meteor.methods({
+  		createHealthCheckResult: function (data) {
+  			Messages.insert({msg: data.data, msgdate: new Date(), server: data.server});
+    		console.log('message posted, count is:' + Messages.find().count());
+    		return {"status":"ok"};
+  		}
+	});
+
+  
 }
