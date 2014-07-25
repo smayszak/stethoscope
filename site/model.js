0914ef061421b55394abef82da449b51b0ffaa0a
jasonmdennis <jasonmdennis@gmail.com>
2014-07-25 16:27:54 -0700

    Create model.js
    new models for the database

---
+++ site/model.js
@@ -0,0 +1,5 @@
+Messages = new Meteor.Collection("messages");
+
+Environments = new Meteor.Collection("environments");
+Products = new Meteor.Collection("products");
+Tests = new Meteor.Collection("tests");
