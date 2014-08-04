c5cc263e36cd53f1150e1c031e1620c041947344
jasonmdennis <jasonmdennis@gmail.com>
2014-08-04 08:59:05 -0700

    new file for environment code

---
+++ site/server/environments.js
@@ -0,0 +1,13 @@
+Meteor.methods({
+
+	post: function(data){
+		//do a check to see if we have the environment in our database already.
+      //add if not.
+      if(Environments.find({name: data}).count()===0)
+      {
+        Environments.insert({name:data, createdAt: new Date()});
+        console.log("failed to find environment " + data + " so it was added");
+      }
+	}
+
+});
